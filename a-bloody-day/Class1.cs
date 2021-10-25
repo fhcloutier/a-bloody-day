d3a65bb789a23c468e87851ee1736d30c89b8002
Francis Harvey Cloutier <francis.harvey-cloutier.1@ens.etsmtl.ca>
2021-10-25 01:08:03 -0400

    Creation of the project

---
+++ a-bloody-day/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+$
+if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+
+namespace Game
+{
+	public class Class1
+	{
+
+	}
+}
